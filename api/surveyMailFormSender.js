3abbdadc0f83d651137c7de552e7c8987901ecb8
FrenckOfficial <onlyfrenckyt@gmail.com>
2026-08-07 22:35:27 +0200

    Aggiunta Google Analytics

--- api/surveyMailFormSender.js
+++ api/surveyMailFormSender.js
@@ -104,6 +104,15 @@ function generateSurveyHtml({ name, email, grade, miglioramenti }) {
 <!DOCTYPE html>
 <html lang="it">
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2LRKW2EXEL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-2LRKW2EXEL');
+  </script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600&display=swap" rel="stylesheet">
