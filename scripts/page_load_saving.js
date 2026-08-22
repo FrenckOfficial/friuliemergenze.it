fb4472210a0206ca3c94b376680211927f1b16da
FrenckOfficial <onlyfrenckyt@gmail.com>
2026-08-22 14:24:55 +0200

    Passaggio alla V3.0.3.0

--- scripts/page_load_saving.js
+++ scripts/page_load_saving.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js';
-import { getFirestore, collection, addDoc, getDocs, query, where } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
 import { firebaseConfig } from '/scripts/firebaseConfig.js';
 
 const app = initializeApp(firebaseConfig);
@@ -14,27 +14,47 @@ async function getIpAddress() {
     console.error("Errore nel recupero IP:", error);
     return "Non disponibile";
   }
-};
+}
+
+async function getCountry(ip) {
+  try {
+    const response = await fetch(`https://ip-api.com/json/${ip}?fields=countryCode`);
+    const data = await response.json();
+    return data.countryCode || null; // es. "US", "IT"
+  } catch (error) {
+    console.error("Errore nel recupero paese:", error);
+    return null;
+  }
+}
 
 const page = window.location.pathname;
 const ipAddress = await getIpAddress();
+const country = await getCountry(ipAddress);
 
 async function createDoc() {
   if (page === "/pagine-visitate" || page === "/pagine-visitate/") {
     return;
-  };
+  }
+
+  console.log(country)
+
+  if (country === "US") {
+    console.log("Visita da IP US, non tracciata.");
+    return;
+  }
 
   try {
     const docRef = await addDoc(collection(db, "pageVisits"), {
       page,
       ipAddress,
+      country,
       timestamp: new Date()
     });
 
     console.log("Documento pagina ", page, " visitata creato con ID:", docRef.id);
   } catch (error) {
-    console.log ("Errore nella creazione del documento:", error);
-  };
-};
+    console.log("Errore nella creazione del documento:", error);
+  }
+}
 
 createDoc();
